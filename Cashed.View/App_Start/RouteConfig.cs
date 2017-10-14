07b895b853a11b05bb07c7fac5a947305efa4dc2
Ivan-Proskurin <ivan.proskurin@gmail.com>
2017-10-14 08:10:01 +0300

    minor changes

--- Cashed.View/App_Start/RouteConfig.cs
+++ Cashed.View/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Cashed.View
@@ -16,7 +12,7 @@ namespace Cashed.View
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Expenses", action = "Add", id = UrlParameter.Optional }
+                defaults: new { controller = "Category", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
